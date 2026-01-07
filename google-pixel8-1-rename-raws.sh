987df96f40f5ef6ab586865b1eba0fada2a1ac44
Yu Shiyang <yu.shiyang@gnayihs.uy>
2026-01-08 03:16:06 +0800

    Renaming and motion photo scripts

---
+++ google-pixel8-1-rename-raws.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+# The stock camera in Google Pixel 8 uses unconventional filenames when saving both JPEG and RAW (DNG) files.
+# It saves them with the suffixes ".RAW-01.COVER.jpg" and ".RAW-02.ORIGINAL.dng" instead of using the same base name with different extensions, as is far more common.
+# This renames both of them to have the suffixes ".RAW.jpg" and ".RAW.dng" in keeping with common practice.
+
+# NOTE: Motion photos with an accompanying RAW file are **NOT** renamed
+# This is by design, as the JPEG file contains additional data and thus should not be subsumed under the RAW file in photo management software
+# Such photos will have the suffixes ".RAW-01.MP.COVER.jpg" and ".RAW-02.ORIGINAL.dng", reflecting their non-overlapping contents
+
+INPUT_DIR="Photos-source/pixel8"
+
+RAW_JPG_SUFFIX=".RAW-01.COVER.jpg"
+RAW_DNG_SUFFIX=".RAW-02.ORIGINAL.dng"
+RAW_MOTION_PHOTO_JPG_SUFFIX=".RAW-01.MP.COVER.jpg"
+
+jpg_counter=0
+no_jpg_raw_photos=()
+no_dng_raw_photos=()
+motion_photo_raw_photos=()
+target_jpg_exist_raw_photos=()
+target_dng_exist_raw_photos=()
+
+while read -r file; do
+	if [ -n "$file" ]; then
+		((jpg_counter++))
+
+		input_file="$INPUT_DIR/$file"
+		input_file_root_name="$(basename "$input_file" "$RAW_JPG_SUFFIX")"
+		input_file_parent_dir="$(dirname "$input_file")"
+		dng_file="$input_file_parent_dir/$input_file_root_name$RAW_DNG_SUFFIX"
+
+		if [ -f "$dng_file" ]; then
+
+			target_jpg_file="$input_file_parent_dir/$input_file_root_name.RAW.jpg"
+			target_dng_file="$input_file_parent_dir/$input_file_root_name.RAW.dng"
+
+			safe_to_rename=1
+
+			if [ -f "$target_jpg_file" ]; then
+				target_jpg_exist_raw_photos+=("$target_jpg_file")
+				safe_to_rename=0
+			fi
+			if [ -f "$target_dng_file" ]; then
+				target_dng_exist_raw_photos+=("$target_dng_file")
+				safe_to_rename=0
+			fi
+
+			if [ $safe_to_rename -eq 1 ]; then
+				echo "Renaming '$input_file' -> '$target_jpg_file'"
+				echo "Renaming '$dng_file' -> '$target_dng_file'"
+				mv "$input_file" "$target_jpg_file"
+				mv "$dng_file" "$target_dng_file"
+			fi
+
+		else
+			no_dng_raw_photos+=("$input_file")
+		fi
+
+	fi
+done <<< "$(find "$INPUT_DIR" -type f -name '*'"$RAW_JPG_SUFFIX" -printf '%P\n')"
+
+while read -r file; do
+	if [ -n "$file" ]; then
+		((jpg_counter++))
+
+		input_file="$INPUT_DIR/$file"
+		input_file_root_name="$(basename "$input_file" "$RAW_DNG_SUFFIX")"
+		input_file_parent_dir="$(dirname "$input_file")"
+		jpg_file="$input_file_parent_dir/$input_file_root_name$RAW_JPG_SUFFIX"
+		motion_photo_jpg_file="$input_file_parent_dir/$input_file_root_name$RAW_MOTION_PHOTO_JPG_SUFFIX"
+
+		if [ ! -f "$jpg_file" ]; then
+			if [ -f "$motion_photo_jpg_file" ]; then
+				motion_photo_raw_photos+=("$input_file")
+			else
+				no_jpg_raw_photos+=("$input_file")
+			fi
+		fi
+
+	fi
+done <<< "$(find "$INPUT_DIR" -type f -name '*'"$RAW_DNG_SUFFIX" -printf '%P\n')"
+
+echo ""
+echo "Renamed $((jpg_counter - ${#no_dng_raw_photos[@]} - ${#target_jpg_exist_raw_photos[@]} - ${#target_dng_exist_raw_photos[@]})) out of $jpg_counter pairs of *$RAW_JPG_SUFFIX and *$RAW_DNG_SUFFIX files"
+
+
+if [ ${#no_dng_raw_photos[@]} -ne 0 ]; then
+	echo ""
+	echo "File pairs not renamed because of the following target jpg file names already exist:"
+	for photo in "${target_jpg_exist_raw_photos[@]}"; do
+		echo "$photo"
+	done
+	echo ""
+fi
+
+if [ ${#no_dng_raw_photos[@]} -ne 0 ]; then
+	echo ""
+	echo "File pairs not renamed because of the following target dng file names already exist:"
+	for photo in "${target_dng_exist_raw_photos[@]}"; do
+		echo "$photo"
+	done
+	echo ""
+fi
+
+if [ ${#motion_photo_raw_photos[@]} -ne 0 ]; then
+	echo ""
+	echo "*$RAW_DNG_SUFFIX files not renamed because it corresponds to a jpg containing a motion photo:"
+	for photo in "${motion_photo_raw_photos[@]}"; do
+		echo "$photo"
+	done
+	echo ""
+fi
+
+if [ ${#no_dng_raw_photos[@]} -ne 0 ]; then
+	echo ""
+	echo "*$RAW_JPG_SUFFIX files not renamed because there is no corresponding dng:"
+	for photo in "${no_dng_raw_photos[@]}"; do
+		echo "$photo"
+	done
+	echo ""
+fi
+
+if [ ${#no_jpg_raw_photos[@]} -ne 0 ]; then
+	echo ""
+	echo "*$RAW_DNG_SUFFIX files not renamed because there is no corresponding jpg:"
+	for photo in "${no_jpg_raw_photos[@]}"; do
+		echo "$photo"
+	done
+	echo ""
+fi
---
+++ google-pixel8-2-extract-motion-photos.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# Extract standalone photo and video files from Google Pixel 8 motion photos
+# Raw photos that are also a motion photo have the suffix ".RAW-01.MP.COVER.jpg"
+
+# Requires:
+# - jq
+# - exiftool, https://exiftool.org/
+# - motionminer from PyPI, patched to extract standalone photos, as in latest commit at https://github.com/yushiyangk/MotionMiner/tree/photo
+
+INPUT_DIR="Photos-source/pixel8"
+OUTPUT_VIDEO_DIR="Photos-source/pixel8_mp_video"
+OUTPUT_PHOTO_DIR="Photos-source/pixel8_mp_photo"
+LIVE_PHOTO_KEYWORD="Motion Photo"
+LIVE_PHOTO_HIERARCHICAL_KEYWORD="__SOURCES|_phones|Google Pixel 8|stock camera|Motion Photo"
+
+RAW_MOTION_PHOTO_JPG_SUFFIX=".RAW-01.MP.COVER.jpg"
+
+motion_photos=()
+counter=0
+
+while read -r file; do
+	if [ -n "$file" ]; then
+		((counter++))
+
+		input_file="$INPUT_DIR/$file"
+		input_file_base_name="$(basename "$input_file" .jpg)"
+		input_file_parent_dir="$(dirname "$input_file")"
+		output_video_file="$OUTPUT_VIDEO_DIR/${file%.jpg}.video.mp4"
+		output_photo_file="$OUTPUT_PHOTO_DIR/${file%.jpg}.photo.jpg"
+		mkdir -p "$(dirname "$output_video_file")"
+		mkdir -p "$(dirname "$output_photo_file")"
+
+		if motionminer "$input_file" -o "$output_video_file" -p "$output_photo_file"; then
+			# Write keyword to original image
+			exiftool \
+				-IPTC:Keywords-="$LIVE_PHOTO_KEYWORD" -IPTC:Keywords+="$LIVE_PHOTO_KEYWORD" \
+				-XMP-dc:Subject-="$LIVE_PHOTO_KEYWORD" -XMP-dc:Subject+="$LIVE_PHOTO_KEYWORD" \
+				-XMP-lr:WeightedFlatSubject-="$LIVE_PHOTO_KEYWORD" -XMP-lr:WeightedFlatSubject+="$LIVE_PHOTO_KEYWORD" \
+				-XMP-lr:HierarchicalSubject-="$LIVE_PHOTO_HIERARCHICAL_KEYWORD" -XMP-lr:HierarchicalSubject+="$LIVE_PHOTO_HIERARCHICAL_KEYWORD" \
+				-IPTCDigest=new -overwrite_original "$input_file"
+
+			# Write keyword to any XMP sidecar files
+			readarray -d '' xmp_files < <( find "$input_file_parent_dir" -type f -name "$input_file_base_name"'.[xX][mM][pP]' -print0 )
+			for xmp_file in "${xmp_files[@]}"; do
+				exiftool \
+					-IPTC:Keywords-="$LIVE_PHOTO_KEYWORD" -IPTC:Keywords+="$LIVE_PHOTO_KEYWORD" \
+					-XMP-dc:Subject-="$LIVE_PHOTO_KEYWORD" -XMP-dc:Subject+="$LIVE_PHOTO_KEYWORD" \
+					-XMP-lr:WeightedFlatSubject-="$LIVE_PHOTO_KEYWORD" -XMP-lr:WeightedFlatSubject+="$LIVE_PHOTO_KEYWORD" \
+					-XMP-lr:HierarchicalSubject-="$LIVE_PHOTO_HIERARCHICAL_KEYWORD" -XMP-lr:HierarchicalSubject+="$LIVE_PHOTO_HIERARCHICAL_KEYWORD" \
+					-IPTCDigest=new -overwrite_original "$xmp_file"
+			done
+
+			# Write keyword to extracted photo and remove motion photo metadata
+			#  Sample directory_json value:
+			#   [
+			#     {
+			#       "Item": {
+			#         "Mime": "image/jpeg",
+			#         "Semantic": "Primary"
+			#       }
+			#     },
+			#     {
+			#       "Item": {
+			#         "Length": 50519,
+			#         "Mime": "image/jpeg",
+			#         "Semantic": "GainMap"
+			#       }
+			#     }
+			#   ]
+			directory_json="$(exiftool -j -struct -XMP-GContainer:ContainerDirectory "$output_photo_file" | jq '.[0].ContainerDirectory | del(.[] | select(.Item.Semantic == "MotionPhoto"))')"
+			directory_struct="$( echo "$directory_json" | jq 'map(.Item | to_entries | map(join("=")) | join(",") | "{Item={" + . + "}}") | join(",") | "[" + . + "]"' --raw-output )"
+			exiftool \
+				-IPTC:Keywords-="$LIVE_PHOTO_KEYWORD" -IPTC:Keywords+="$LIVE_PHOTO_KEYWORD" \
+				-XMP-dc:Subject-="$LIVE_PHOTO_KEYWORD" -XMP-dc:Subject+="$LIVE_PHOTO_KEYWORD" \
+				-XMP-lr:WeightedFlatSubject-="$LIVE_PHOTO_KEYWORD" -XMP-lr:WeightedFlatSubject+="$LIVE_PHOTO_KEYWORD" \
+				-XMP-lr:HierarchicalSubject-="$LIVE_PHOTO_HIERARCHICAL_KEYWORD" -XMP-lr:HierarchicalSubject+="$LIVE_PHOTO_HIERARCHICAL_KEYWORD" \
+				-XMP-GContainer:ContainerDirectory="$directory_struct" \
+				-IPTCDigest=new -overwrite_original "$output_photo_file"
+
+			motion_photos+=("$input_file")
+		fi
+
+	fi
+done <<< "$(find "$INPUT_DIR" -type f -name '*'"$RAW_MOTION_PHOTO_JPG_SUFFIX" -printf '%P\n')"
+
+find "$OUTPUT_VIDEO_DIR" -type d -empty -delete
+find "$OUTPUT_PHOTO_DIR" -type d -empty -delete
+
+echo ""
+if [ ${#motion_photos[@]} -ne 0 ]; then
+	echo "Found and extracted ${#motion_photos[@]} motion photos out of $counter original *.jpg files:"
+	for photo in "${motion_photos[@]}"; do
+		echo "$photo"
+	done
+	echo ""
+	echo "Extracted to $OUTPUT_VIDEO_DIR and $OUTPUT_PHOTO_DIR"
+else
+	echo "Found and extracted 0 motion photos out of $counter original *.jpg files"
+fi
